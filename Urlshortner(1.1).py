132fccc22debd5912dc22c64d9d8ac0c1d7e347f
Akshay Ravi <58636452+AkshayraviC09YC47@users.noreply.github.com>
2020-04-17 20:06:55 +0530

    Create Urlshortner(1.1).py

---
+++ Urlshortner(1.1).py
@@ -0,0 +1,28 @@
+import os
+import requests
+
+os.system("clear")
+
+print("""                                   
+--.--o                        |    
+  |  .,---.,   .    .   .,---.|    
+  |  ||   ||   |    |   ||    |    
+  `  ``   '`---|    `---'`    `---'
+           `---'                   """)
+           
+print("<--------Coded By Copycat-------->")
+print("")
+
+site = input("[+] Site Name: ")
+apiurl = "https://tinyurl.com/api-create.php?url="
+result = apiurl + site
+
+def tinyurl():
+	try:
+		r = requests.get(result)
+		r.raise_for_status()
+		print("[+] " +r.text)
+	except:
+		print("[-] oops..!! Error Or Invalid Site")
+	
+tinyurl()
